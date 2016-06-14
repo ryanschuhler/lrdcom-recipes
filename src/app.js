5063ac9912fab5c2258b575f5ced4d5540914548
Phillip_Chan <phillip.chan@liferay.COM>
2016-06-14 10:58:54 -0700

    HTML5 URL Testing

--- src/app.js
+++ src/app.js
@@ -816,6 +816,9 @@ var routes = (function() {
 			$body.hide();
 			section.load('src/pages/' + page + '.html', function() {
 				$body.fadeIn(1000);	
+				if (alterHistory) {
+					history.pushState(page, null, page);	
+				}
 			});
 			
 		} 
@@ -825,7 +828,7 @@ var routes = (function() {
 					section.load('src/pages/' + page + '.html', function() {
 						section.fadeIn(350);
 						if (alterHistory) {
-							history.pushState(page, null, '#/' + page);	
+							history.pushState(page, null, page);	
 						}
 					})	
 				}, 300)
@@ -840,15 +843,28 @@ var routes = (function() {
 		navigationUI.findActiveNav(e.state)
 	})
 
-	// when users load friendly url, got o that url
-	var hash = window.location.hash;
 
-	if (hash === "#/" || hash.length === 0) {
-		changePage('home', "init");	
-	} else {
-		hash = hash.replace("#/", "");
-		changePage(hash, "init");
-	}
+	// load page to correct routes on initial load
+	var initialRoute = (function() {
+		if (!sessionStorage.redirect) {
+			changePage('home', "init");
+		} else {
+			let url = sessionStorage.redirect;
+			var page = url.slice(url.lastIndexOf('/') + 1, url.length);
+			alert('changing to page: ' + page);
+			changePage(page, "init");
+		}
+	})();
+
+	// when users load friendly url, got o that url
+	// var hash = window.location.hash;
+
+	// if (hash === "#/" || hash.length === 0) {
+	// 	changePage('home', "init");	
+	// } else {
+	// 	hash = hash.replace("#/", "");
+	// 	changePage(hash, "init");
+	// }
 
 	return {
 		changePage: changePage
--- src/js/routes.js
+++ src/js/routes.js
@@ -22,6 +22,9 @@ var routes = (function() {
 			$body.hide();
 			section.load('src/pages/' + page + '.html', function() {
 				$body.fadeIn(1000);	
+				if (alterHistory) {
+					history.pushState(page, null, page);	
+				}
 			});
 			
 		} 
@@ -31,7 +34,7 @@ var routes = (function() {
 					section.load('src/pages/' + page + '.html', function() {
 						section.fadeIn(350);
 						if (alterHistory) {
-							history.pushState(page, null, '#/' + page);	
+							history.pushState(page, null, page);	
 						}
 					})	
 				}, 300)
@@ -46,15 +49,28 @@ var routes = (function() {
 		navigationUI.findActiveNav(e.state)
 	})
 
-	// when users load friendly url, got o that url
-	var hash = window.location.hash;
 
-	if (hash === "#/" || hash.length === 0) {
-		changePage('home', "init");	
-	} else {
-		hash = hash.replace("#/", "");
-		changePage(hash, "init");
-	}
+	// load page to correct routes on initial load
+	var initialRoute = (function() {
+		if (!sessionStorage.redirect) {
+			changePage('home', "init");
+		} else {
+			let url = sessionStorage.redirect;
+			var page = url.slice(url.lastIndexOf('/') + 1, url.length);
+			alert('changing to page: ' + page);
+			changePage(page, "init");
+		}
+	})();
+
+	// when users load friendly url, got o that url
+	// var hash = window.location.hash;
+
+	// if (hash === "#/" || hash.length === 0) {
+	// 	changePage('home', "init");	
+	// } else {
+	// 	hash = hash.replace("#/", "");
+	// 	changePage(hash, "init");
+	// }
 
 	return {
 		changePage: changePage
