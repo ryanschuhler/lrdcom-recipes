7707e717f6c2deea2c1e6c705493b8ac24a9b723
allen-ziegenfus <allen.ziegenfus@liferay.com>
2016-08-29 18:16:55 +0200

    work on italy speakers section

---
+++ 6.1.x/events/2016/italy/test.sh
@@ -0,0 +1,5 @@
+
+phantomjs test/italy.js italy-home https://www.liferay.com/web/events2016/italy/home &
+phantomjs test/italy.js italy-agenda https://www.liferay.com/web/events2016/italy/agenda &
+phantomjs test/italy.js italy-location https://www.liferay.com/web/events2016/italy/location &
+phantomjs test/italy.js italy-sponsors https://www.liferay.com/web/events2016/italy/sponsors &
