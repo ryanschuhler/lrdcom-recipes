173dbf0c1e8a8c846137bc9c93c826e8c3213202
ryanschuhler <ryan.schuhler@liferay.com>
2018-08-16 07:30:39 -0700

    Remove key

--- 6.2.x/csv-to-hubspot/src/js/config.js
+++ 6.2.x/csv-to-hubspot/src/js/config.js
@@ -4,7 +4,7 @@ var config = (
 			cycle_duration: 1500,
 			hubspotFormID: '231dd439-b782-4e95-9cee-776e9a29d3f2',
 			hubspotPortalID: '252686',
-			hubspotAPIKey: 'cb8584d4-f2e9-4b2f-bd5d-1ca9a032bcc2',
+			hubspotAPIKey: 'xxx',
 			stepsContainerClass: 'steps-container',
 			uploadContainerClass: 'file-drag',
 			updateConfig: function(key, value) {
